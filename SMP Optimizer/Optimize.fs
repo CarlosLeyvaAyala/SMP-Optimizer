afd550675d30b100d0b6279383203fa0692af148
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2024-02-03 23:16:50 -0600

    Add algorithm

---
+++ SMP Optimizer/Optimize.fs
@@ -0,0 +1,62 @@
+﻿module Optimize
+
+open System.Collections.Generic
+open System.IO
+open DMLib
+open DMLib.String
+
+let private splitByOkAndErrors xs =
+    let oks = List<'T>()
+    let errors = List<'V>()
+
+    for x in xs do
+        match x with
+        | Ok v -> oks.Add v
+        | Error e -> errors.Add e
+
+    (oks |> seq |> List.ofSeq, errors |> seq |> List.ofSeq)
+
+let private displayFileList title lst =
+    match lst with
+    | [] -> ()
+    | l ->
+        printfn "%s" title
+        "".PadRight(30, '=') |> printfn "%s"
+        l |> List.iter (printfn "%s")
+
+let private replace (filename, contents) =
+    if contents |> contains "per-triangle-shape" then
+        Some <| (filename, contents |> replace "per-triangle-shape" "per-vertex-shape")
+    else
+        None
+
+let private read fn = fn |> Tuple.dupMapSnd File.ReadAllText
+
+let private write r (filename: string, contents: string) =
+    try
+        //File.WriteAllText(fn, contents)
+        filename |> r |> Ok
+    with e ->
+        Error e.Message
+
+let singleFile filename =
+    match filename |> read |> replace |> Option.map (write id) with
+    | None -> printfn "No optimizations were needed"
+    | Some(Ok _) -> printfn "Optimization was successful"
+    | Some(Error e) -> printfn "Could not optimize: %s" e
+
+let directory basePath =
+    let r (s: string) = s.Replace(basePath, "")
+
+    let ok, errors =
+        Directory.GetFiles(basePath, "*.xml", SearchOption.AllDirectories) // Find files
+        |> Array.map read // Get file contents
+        |> Array.Parallel.choose replace // Replacement step
+        |> Array.map (write r) // Save updated files
+        |> splitByOkAndErrors
+
+    match ok, errors with
+    | [], [] -> printfn "No optimizations were needed"
+    | _, _ ->
+        ok |> displayFileList "The following files were optimized successfully"
+        errors |> displayFileList "Errors found while optimizing"
--- SMP Optimizer/Program.fs
+++ SMP Optimizer/Program.fs
@@ -1,2 +1,49 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open DMLib.String
+open DMLib.IO
+open DMLib.IO.Path
+
+[<EntryPoint>]
+let main (args) =
+    Console.Title <- "SMP Optimizer"
+
+    match args with
+    | [||] ->
+        let smartassBanner t =
+            let b = "".PadRight(70, '*')
+            printfn "%s" b
+            printfn "%s" t
+            printfn "%s" b
+
+        """
+Usage: drag and drop at least one folder to this program.
+
+Try again."""
+        |> trim
+        |> smartassBanner
+    | a ->
+        let p = printfn "%s"
+
+        let banner t =
+            let b = "".PadRight(90, '*')
+            p "\n"
+            p b
+            p $"Processing:\n{t}"
+            p b
+            p ""
+
+        a
+        |> Array.iter (fun dir ->
+            match dir with
+            | IsDir d ->
+                banner d
+                Optimize.directory d
+            | FileExists f & (IsExtension "xml" _) ->
+                banner f
+                Optimize.singleFile f
+            | x -> banner $"{x} can not be processed.")
+        |> ignore
+
+    printfn "\nPress any key to continue..."
+    Console.ReadKey() |> ignore
+    0
